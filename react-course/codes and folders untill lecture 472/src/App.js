ec5f08bee3fbf554a2960401a14b4c6d3523cfcd
moonctp24 <67907517+moonctp24@users.noreply.github.com>
2021-11-08 16:49:57 +0900

    arrange folders for new lecture, 473

---
+++ react-course/codes and folders untill lecture 472/src/App.js
@@ -0,0 +1,21 @@
+import Todo from './components/Todo'
+import NewExpense from './NewExpense/NewExpense';
+
+function App() {
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  }
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React' />
+      <Todo text='Master React'/>
+      <NewExpense onAddExpense={addExpenseHandler}/>
+    </div>
+  );
+}
+
+export default App;
+
+// delete newExpense after section 4
