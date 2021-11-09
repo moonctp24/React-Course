e2117ebe662431db7ef5aa3bc3a8e362a930cee1
moonctp24 <67907517+moonctp24@users.noreply.github.com>
2021-11-09 13:13:43 +0900

    study until lecture 476

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,5 +1,6 @@
 import {Route, Switch} from 'react-router-dom';
 
+import MainNavigation from './components/layout/MainNavigation';
 import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupPage from './pages/NewMeetup';
@@ -7,6 +8,7 @@ import NewMeetupPage from './pages/NewMeetup';
 
 function App() {
   return <div>
+    <MainNavigation />
     <Switch>
       <Route path='/' exact={true}>
         <AllMeetupsPage />
