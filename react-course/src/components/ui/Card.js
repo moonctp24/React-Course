38acd2c48a2974139f5f9f0828103f098148e6fb
moonctp24 <67907517+moonctp24@users.noreply.github.com>
2021-11-14 15:48:50 +0900

    Card UI에 안뜨는 것 수정

--- react-course/src/components/ui/Card.js
+++ react-course/src/components/ui/Card.js
@@ -1,7 +1,7 @@
 import classes from './Card.module.css';
 
 function Card(props) {
-    return <div className={classes.Card}>
+    return <div className={classes.card}>
         {props.children}
     </div>
 }
